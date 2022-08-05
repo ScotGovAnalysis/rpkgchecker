639a6254ccfb26ec26f15344b1e2886d42cef84f
tomwilsonsco <thomaswilson81@gmail.com>
2022-08-05 15:31:55 +0100

    get server function

---
+++ R/get_server_packages.R
@@ -0,0 +1,14 @@
+get_server_packages <- function(server_package_dir) {
+  server_packages <- list.files(packages_dir, pattern = "*") %>%
+    as_tibble() %>%
+    rename(server_file_name = value)
+
+  # Clean server packages
+  server_packages <- server_packages %>%
+    separate(server_file_name, sep = "_", into = c("server_package", "server_version"), extra = "merge", fill = "right", remove = TRUE) %>%
+    mutate(
+      server_version = str_replace_all(server_version, "-", "."),
+      server_version = str_replace_all(server_version, ".zip", "")
+    )
+  return(server_packages)
+}
