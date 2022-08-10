ce2d8c3bc114cb6aa4ce806708dab47b87043d97
tomwilsonsco <thomaswilson81@gmail.com>
2022-08-10 10:57:29 +0100

    desc of script purpose

--- run_checks.R
+++ run_checks.R
@@ -1,8 +1,12 @@
+# This simply generates the datasets that are used for manual checking and other processes
+# To generate the html report for a particular package run create_rpackage_report.R
+
 source("R/get_required_packages.R") # filter cleaned
 source("R/clean_available_packages.R") # custom function to clean utils::available.packages() df
 source("R/check_r_version_required.R")# check version of R is >= to min R required for packages
 source("R/get_server_packages.R")
 source("R/compare_available_server_packages.R")
+source("R/utils.R")
 
 # Name of package to search, e.g. tidyr, dplyr, stringr etc
 search_package <- "MatchIt"
@@ -22,5 +26,7 @@ server_packages <- get_server_packages(package_dir)
 
 package_requirements_checked <- compare_available_server_packages(package_requirements, server_packages)
 
+custom_sort_package_df(search_package, package_requirements_checked, "package")
+
 # Write the results to csv
-# write_csv(package_requirements, file.path("outputs", paste0(search_package, "_requirements.csv")))
+write_csv(package_requirements, file.path("outputs", paste0(search_package, "_requirements.csv")))
