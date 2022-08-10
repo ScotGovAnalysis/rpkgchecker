2883722ddad9a8382e006829d084484000c84b81
tomwilsonsco <thomaswilson81@gmail.com>
2022-08-10 10:59:13 +0100

    function for report creation

---
+++ R/generate_rpackage_report.R
@@ -0,0 +1,15 @@
+generate_rpackage_report <- function(search_package, server_dir) {
+  # Set the name of the output html file
+  output_html_file <- paste0("package_requirements_", search_package, ".html")
+  
+  # Render the existing markdown report, passing in the search package and server directory
+  # Save the resulting html in the outputs directory
+  render(file.path("markdown", "package_checks.Rmd"),
+         output_file = output_html_file,
+         output_dir = "outputs",
+         params = list(
+           package_to_check = search_package,
+           server_dir = server_dir
+         )
+  )
+}
