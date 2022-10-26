af9868bd5e05cb0324247763a879bfc9658a2e98
tomwilsonsco <thomaswilson81@gmail.com>
2022-10-26 13:45:26 +0100

    roxygen and fix

--- R/check_r_version_required.R
+++ R/check_r_version_required.R
@@ -1,18 +1,30 @@
-library(crayon)
-check_r_version_required <- function(package_requirements_df) {
+
+#' Title
+#'
+#' @param required_packages Tibble of required packages.
+#'
+#' @return Tibble of required packages that require R > existing installed
+#' version. Or FALSE if no required packages exceed existing R install version.
+#' @export
+#'
+#' @examples
+#' available_long_tb <- available_packages_long()
+#' required_tb <- search_required_packages(available_long_tb, "fabletools")
+#' exceed_r_version <- check_r_version(required_tb)
+check_r_version <- function(required_packages) {
   # extract R version from column
-  r_requirements <- package_requirements_df %>%
-    separate(r_version,
+  r_requirements <- required_packages %>%
+    tidyr::separate(.data$r_version,
       sep = "\\(", into = c("r", "r_install_version"), remove = FALSE, extra = "merge",
       fill = "right"
     ) %>%
-    separate(r_install_version,
+    tidyr::separate(.data$r_install_version,
       sep = "(?=\\d)", into = c("r_comparator", "r_install_version"), remove = TRUE, extra = "merge",
       fill = "right"
     ) %>%
-    mutate(r_install_version = str_replace_all(r_install_version, "\\)", ""))
+    dplyr::mutate(.data$r_install_version = stringr::str_replace_all(.data$r_install_version, "\\)", ""))
   r_current_version <- paste0(R.Version()$major, ".", R.Version()$minor)
-  exceed_version <- r_requirements %>% filter(r_install_version > r_current_version)
+  exceed_version <- r_requirements %>% filter(.data$r_install_version > .data$r_current_version)
   if (nrow(exceed_version) > 0) {
     return(exceed_version)
   } else {
