d03bda8a8ef271311f7e305704e89cfd38940ac0
tomwilsonsco <thomaswilson81@gmail.com>
2022-11-29 12:23:12 +0000

    package not exist check

--- R/search_requirements.R
+++ R/search_requirements.R
@@ -23,11 +23,15 @@
 search_requirements <- function(packages_long,
                                 package_name,
                                 package_version_number = NA) {
+  if (!package_name %in% packages_long$package) {
+    stop(format_message(paste("search package", package_name, "not found in
+                       available packages tibble")))
+  }
   if ((!(is.na(package_version_number)) & (
     !stringr::str_detect(package_version_number,
       pattern = "(\\d+)\\.(\\d+)\\.(\\d+)"
     )))) {
-    stop(strwrap(paste(
+    stop(format_message(paste(
       package_version_number,
       "is not a valid version number. Set as NA if latest compatible
        version required."
@@ -56,7 +60,7 @@ search_requirements <- function(packages_long,
   # warn if search version != available version
   if (!is.na(package_version_number) &
     (pkg_available_version != package_version_number)) {
-    warning(strwrap(paste(
+    warning(format_message(paste(
       package_name, "version", package_version_number,
       "is not available in the CRAN repository searched. Version",
       pkg_available_version, "is available."
